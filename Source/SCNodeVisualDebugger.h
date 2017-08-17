56469705a233d3ac33995e33b707c317dfb78efe
Andrey Arzhannikov <andreya@handsome.is>
2017-08-17 16:37:16 +0600

    Setup project

---
+++ Source/SCNodeVisualDebugger.h
@@ -0,0 +1,19 @@
+//
+//  SCNodeVisualDebugger.h
+//  SCNodeVisualDebugger
+//
+//  Created by Andrey Arzhannikov on 17.08.17.
+//  Copyright © 2017 Handsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCNodeVisualDebugger.
+FOUNDATION_EXPORT double SCNodeVisualDebuggerVersionNumber;
+
+//! Project version string for SCNodeVisualDebugger.
+FOUNDATION_EXPORT const unsigned char SCNodeVisualDebuggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCNodeVisualDebugger/PublicHeader.h>
+
+
