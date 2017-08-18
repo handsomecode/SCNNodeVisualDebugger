d09f1e6731f7c98a70208742cb792848475761e6
Andrey Arzhannikov <andreya@handsome.is>
2017-08-18 13:06:06 +0600

    Add SceneKit sample

---
+++ SceneKitSample/SceneKitSample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SceneKitSample
+//
+//  Created by Andrey Arzhannikov on 18.08.17.
+//  Copyright © 2017 Handsome. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
