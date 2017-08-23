5cedd241178f52e52b7edd353098b5b572c56e7b
Andrey Arzhannikov <andreya@handsome.is>
2017-08-23 16:52:03 +0600

    Remove commented code

--- SceneKitSample/SceneKitSample/DemoViewController.swift
+++ SceneKitSample/SceneKitSample/DemoViewController.swift
@@ -22,22 +22,16 @@ import SCNNodeVisualDebugger
 
 class GameViewController: UIViewController {
     
-    let scene = SCNScene(named: "art.scnassets/ship.scn")!
-    lazy var earthNode: SCNNode = {
+    private let scene = SCNScene(named: "art.scnassets/ship.scn")!
+    
+    private lazy var earthNode: SCNNode = {
         guard let earth = self.scene.rootNode.childNode(withName: "Earth", recursively: true) else {
             preconditionFailure("Earth node not found")
         }
         return earth
     }()
     
-//    lazy var moonWrapperNode: SCNNode = {
-//        guard let earth = scene.rootNode.childNode(withName: "MoonWrapper", recursively: true) else {
-//            preconditionFailure("MoonWrapper node not found")
-//        }
-//        return earth
-//    }()
-    
-    lazy var moonNode: SCNNode = {
+    private lazy var moonNode: SCNNode = {
         guard let earth = self.scene.rootNode.childNode(withName: "Moon", recursively: true) else {
             preconditionFailure("Moon node not found")
         }
@@ -51,9 +45,13 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //earth rotation animation
         earthNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 5)))
         
+        //move Moon node to orbit
         moonNode.pivot = SCNMatrix4MakeTranslation(0, 0, 3)
+        
+        //Moon rotation animation
         moonNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 5)))
         
         // retrieve the SCNView
@@ -70,31 +68,16 @@ class GameViewController: UIViewController {
         
         // configure the view
         scnView.backgroundColor = UIColor.black
-        scnView.enableDebugAxesByDoubleTap = true
         
-        // add a tap gesture recognizer
-//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-//        tapGesture.numberOfTapsRequired = 2
-//        scnView.addGestureRecognizer(tapGesture)
+        //enable debugging on specific node by double tap by that
+        scnView.enableDebugAxesByDoubleTap = true
     }
     
-//    @objc
-//    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-//        // retrieve the SCNView
-//        let scnView = self.view as! SCNView
-//
-//        // check what nodes are tapped
-//        let p = gestureRecognize.location(in: scnView)
-//        let hitResults = scnView.hitTest(p, options: [:])
-//        // check that we clicked on at least one object
-//        if hitResults.count > 0 {
-//            // retrieved the first clicked object
-//            let result = hitResults[0]
-//            if result.node.hasDebugAxes() {
-//                result.node.removeDebugAxes()
-//            } else {
-//                result.node.addDebugAxes()
-//            }
-//        }
-//    }
+    private func switchDebugging(on node: SCNNode) {
+        if node.hasDebugAxes() {
+            node.removeDebugAxes()
+        } else {
+            node.addDebugAxes()
+        }
+    }
 }
